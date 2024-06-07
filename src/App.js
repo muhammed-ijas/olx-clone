a55e9a778fc6cc5bd582776bb2e4067733feeeeb
Muhammed Ijas T <muhammedijas793@gmail.com>
2024-06-07 18:26:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+
+import {BrowserRouter as Router , Route} from 'react-router-dom'
+import Signup from './Pages/Signup'
 import './App.css';
 
 /**
@@ -9,9 +12,18 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Route exact path='/' >
+        <Home/>
+        </Route>
+
+        <Route path='/signup' >
+          <Signup/>
+        </Route>
+      </Router>
     </div>
   );
 }
 
 export default App;
+ 
